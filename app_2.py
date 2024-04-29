47d5811539a1fbd4aed82d741ac354e9f8313942
jessica-nadeak <167520807+jessica-nadeak@users.noreply.github.com>
2024-04-29 19:43:57 +0700

    Add files via upload

---
+++ app_2.py
@@ -0,0 +1,67 @@
+from numpy import array
+import numpy as np
+import streamlit as st 
+import folium 
+from streamlit_folium import st_folium , folium_static # Untuk membuat MAP
+from streamlit_option_menu import option_menu # Untuk membuat SIDE BAR
+import pandas as pd
+import PIL.Image as img
+import time
+import pickle
+from time_predict import run_ml
+
+#Tampilan halaman utama
+st.title('Porter Delivery Time Estimation')
+
+
+image = img.open('porterimg2.jpg')
+st.image(image)
+
+
+desc_1 = """
+        Porter is a tech-enabled logistics company offering a variety of intracity 
+        and intercity delivery services. From a pin to an entire house, Porter will 
+        deliver anything, anywhere, anytime. However, Porter does not deliver item(s) 
+        which are illegal, dangerous or hazardous in nature or which are prohibited by 
+        any statue or law. 
+        """
+
+desc_2 = """
+        This app is made, as a Final Project from Digital Skola, by Team Honeycomb. 
+        This app will do Delivery Time Estimation on Porter delivery services. 
+        The whole process upon making this app started from thorough data preprocessing, 
+        model selection & training, model evaluation until model tuning. We believe the 
+        prediction the app generates is accurate as we made it from the best of our knowledge.
+        """
+df = pd.DataFrame({'Team Member': 
+                   ['Jessica Manna Febriani Nadeak',
+                    'Ignatius Evans Erlangga',
+                    'Hapsari Warih Utami',
+                    'Feri Aditya Ridwan Mas',
+                    'Feliks Wijaya Santoso',
+                    'Fela Suvah'
+                    ]})
+
+
+page = option_menu('MENU' , ['About Porter', 'About Us'])
+if page == 'About Porter':
+   st.caption(desc_1)
+elif page == 'About Us':
+   st.caption (desc_2)
+   st.subheader('Team Honeycomb consists of :')
+   st.dataframe(df.style.hide())
+
+def main():
+   menu = ['Home', 'Delivery Time Prediction']
+   choice = st.sidebar.selectbox('Menu', menu)
+    
+   if choice == 'Home':
+      print('')
+   elif choice == 'Delivery Time Prediction':
+      run_ml()
+    
+if __name__== '__main__':
+   main()
+
+    
+
---
+++ time_predict.py
@@ -0,0 +1,110 @@
+import streamlit as st
+import numpy as np
+
+#load ML Package
+import joblib
+import os
+import xgboost as xgb
+import LinearRegression
+
+attribut_info = """
+                - Total Item: How many food you ordered
+                - Market_ID: Restaurant Location
+                - Order_Protocol: How do you order
+                - Store_Primary_Category: What kind of restaurant
+                """
+
+mar_id = {'Region 1': 1,
+          'Region 2': 2,
+          'Region 3': 3,
+          'Region 4': 4,
+          'Region 5': 5,
+          'Region 6': 6
+          }
+
+ord_pro = {'Through Porter': 1,
+           'Call to Restaurant' : 2,
+           'Pre-booked': 3,
+           'Third Party': 4,
+           'Others': 5,
+           'Others': 6,
+           'Others': 7,
+            }
+
+sto_ctgr = {'Ethnic Based Food': 1,
+            'Specialized Food': 2,
+            'Dietary Based Food': 3,
+            'Others': 4
+            }
+
+def get_value(val,my_dict):
+    for key,value in my_dict.items():
+        if val == key:
+            return value
+
+
+
+#def predict_delivery_time(total_item, market_id, order_protocol, restaurant_category):
+    # Menggunakan rumus sederhana untuk estimasi waktu pengiriman
+    #estimated_time = model_.predict([[total_item, market_id, order_protocol, restaurant_category]])
+    #return estimated_time
+
+
+@st.cache_data
+def load_model (model):
+    loaded_model = joblib.load(open(os.path.join(model),'rb'))
+    return loaded_model
+
+def run_ml():
+    st.subheader('Your Food Delivery Time')
+    st.caption('Please input these information for predict')
+    with st.expander('What do I need to input?'):
+        st.markdown(attribut_info)
+    
+    total_item = st.number_input('How many food did you order?', 1, 500,)
+    total_distinct = st.number_input('How many types of food did you order', 1, 500)
+    subtotal = st.number_input('How much does your food cost', 1, 30000)
+    market_id = st.selectbox('Where the restaurant located at?',
+                       ['Region 1', 'Region 2', 'Region 3', 'Region 4', 'Region 5','Region 6'])
+    order_protocol = st.selectbox('How did you order the Food?', ['Through Porter', 'Call to Restaurant', 'Pre-booked', 'Third Party',
+                                    'Others'])
+    rest_cate = st.selectbox('What kind of food the restaurant sell?', ['Ethnic Based Food','Specialized Food',
+                                 'Dietary Based Food','Others'])
+    
+    with st.expander("Your Selected Options"):
+        result = {
+            'Total Item': total_item,
+            'Total Types of Item': total_distinct,
+            'Price': subtotal,
+            'Location': market_id,
+            'Order by': order_protocol,
+            'Restaurant Type':rest_cate
+            }
+
+    encoded_result = []
+    for i in result.values():
+        if type(i) == int:
+            encoded_result.append(i)
+        elif i in ['Region 1', 'Region 2', 'Region 3', 'Region 4', 'Region 5', 'Region 6']:
+            res = get_value(i, mar_id)
+            encoded_result.append(res)
+        elif i in ['Through Porter', 'Call to Restaurant', 'Pre-booked', 'Third Party','Others']:
+            res = get_value(i, ord_pro)
+            encoded_result.append(res)
+        elif i in ['Ethnic Based Food','Specialized Food','Dietary Based Food','Others', 'Others', 'Others']:
+            res = get_value(i, sto_ctgr)
+            encoded_result.append(res)
+    
+    single_sample = np.array(encoded_result).reshape(1,-1)
+    
+    model_ = load_model('linear_regression_model.pkl')
+
+    predict = model_.predict(single_sample)
+
+    st.write(predict)
+    
+    st.subheader('Your Foods Are Arriving in')
+    if st.button('Predict Delivery Time'):
+        # Prediksi waktu pengiriman
+        predict = predict/60
+        st.success(f"Estimated Delivery Time: {predict} minutes")
